89c0a9e9a32b01f16b085d61c4edaf1e4d6dec4f
Saadetatabay <etatabaysaad143@gmail.com>
2025-12-10 14:24:09 +0300

    powerset tam

--- powerset/main.c
+++ powerset/main.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void is_subset(int count,int substoplam,int *ar)
+void print_array(int temp_arr[], int size) {
+    printf("COZUM: [ ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", temp_arr[i]);
+    }
+    printf("]\n");
+
+}
+
+void is_subset(int count,int substoplam,int ar[],int temp_arr[],int size,int temp_toplam,int indis)
 {
+    int i;
+    if (temp_toplam == substoplam)
+    {
+        print_array(temp_arr,size);
+        //return ;
+    }
+    i = indis;
+    while (i < count)
+    {
+        temp_arr[size] = ar[i]; // ilk elemanı ekledik
     
+        is_subset(count,substoplam,ar,temp_arr,size+1,temp_toplam+ar[i],i+1);
+        i++;
+    }
 }
 
 int main(int argc,char *argv[])
@@ -14,10 +36,11 @@ int main(int argc,char *argv[])
     int subs_toplam = atoi(argv[1]); //subsettoplamı olması gerek
     int count = argc - 2;
     int num[count];
+    int temp_dizei[count];
     while (i < count)
     {
         num[i] = atoi(argv[i+2]);
         i++;
     }
-    is_subset(count,subs_toplam,num);
+    is_subset(count,subs_toplam,num,temp_dizei,0,0,0);
 }
