6104e4ae7877fbaf3001b6dc4babeca09ae102f5
Saadetatabay <etatabaysaad143@gmail.com>
2025-12-10 11:47:30 +0300

    perm words tam

--- permutaionword/main.c
+++ permutaionword/main.c
@@ -1,9 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "perm.h"
 
 void    permutations(char   *argv)
 {
     int len = ft_strlen(argv);
+    permute(argv,0,len);
+}
+void permute (char *s,int indis,int end)
+{
+    int i;
+    if(indis == end)
+    {
+        puts(s);
+        return ;
+    }
+    i = indis;
+    while (i < end)
+    {
+        // ab olsun indis 0 end 2 
+        // i = 0
+        // permute(s,1,2)
+        //i = 1
+        //ab puts
+        // i = 1
+        //ba permute(s,2,2) ba puts
+        swap(&s[indis],&s[i]); // indis başta 0 i ile baştan sona tüm elemanlar 0. indise geliyor
+        permute(s,indis+1,end); // 1. konuma geçtik i bu sefer ende kafar tüm sayıları 1. konuma getirdi
+        swap(&s[indis],&s[i]);
+        i++;
+    }
+}
+void    swap(char *a, char *b)
+{
+    char temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int ft_strlen(char *ar)
---
+++ permutaionword/perm.h
@@ -0,0 +1,12 @@
+#ifndef PERM_H
+#define PERM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_strlen(char *ar);
+void    swap(char *a, char *b);
+void permute (char *s,int indis,int end);
+void    permutations(char   *argv);
+
+#endif
