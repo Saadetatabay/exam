585d745578b3994e6d30ec44c258cfc634b997ab
Saadetatabay <etatabaysaad143@gmail.com>
2026-01-16 12:51:42 +0300

    powerset güncel

---
+++ powerset/power.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int ac;
+char **av;
+int target;
+int solution = 0;
+int *subset;
+int subsetlen = 0;
+
+void print()
+{
+	for(int i = 0; i < subsetlen; i++)
+	{
+		solution = 1;
+		if (i > 0)
+			printf(" ");
+		printf("%d", subset[i]);
+	}
+	printf("\n");
+}
+
+int sum()
+{
+	int sum = 0;
+	for(int i = 0; i < subsetlen; i++)
+		sum += subset[i];
+	return sum;
+}
+
+
+void find(int start)
+{
+	if (subsetlen != 0 && sum() == target)
+		print();
+
+	for (int i = start; i < ac; i++)
+	{
+		subset[subsetlen++] = atoi(av[i]);
+		find(i + 1);
+		subsetlen--;
+	}
+}
+
+
+
+int main(int argc, char **argv)
+{
+	if (argc <= 2)
+		return 1;
+	ac = argc;
+	av = argv;
+	target = atoi(av[1]);
+	int buf[argc - 2];
+	subset = buf;
+
+	find(2);
+	if (!solution)
+		printf("\n");
+
+}
